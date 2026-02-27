1cf300fc30a97d3314e5f71f5993358efe3ef3e4
Rychtrovka <info@rychtrovka.cz>
2026-02-27 04:18:27 +0100

    vynuceni bileho pozadi a kontrola layoutu

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 ﻿export default function Home() {
-  return (
-    <main style={{ height: "100vh", width: "100vw" }}>
-      {/* sem dej, co chceš jako úvod */}
-      <a href="/tv">Otevřít TV</a>
-    </main>
-  )
+    return (
+        <main style={{
+            height: "100vh",
+            width: "100vw",
+            background: "white",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+        }}>
+            <div style={{ textAlign: "center", color: "black" }}>
+                <h1>Rychtrovka Kiosk</h1>
+                <a href="/tv" style={{
+                    padding: "10px 20px",
+                    background: "#0070f3",
+                    color: "white",
+                    borderRadius: "5px",
+                    textDecoration: "none"
+                }}>
+                    Otevřít TV
+                </a>
+            </div>
+        </main>
+    )
 }
